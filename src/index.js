e2e5a0957fc3c67b6c1891cf5871f43b7a372a95
lucasjunior24 <lukateku@gmail.com>
2021-02-02 13:14:10 -0300

    Definindo rotas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const router = require('./Utils/router')
 const app = express()
 
 app.use(express.json())
-app.get('/', (req, res) => {
-    return res.send('Hellor word')
-})
+app.use(router)
+
+
 
 app.listen(3333, () => console.log('Servdido rodando na porta 33333'))
