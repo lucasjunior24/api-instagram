271ac64d80c355f653efa84ae4d8f98628384ead
lucasjunior24 <lukateku@gmail.com>
2021-02-02 13:39:20 -0300

    Conectando ao banco de dados

--- src/Utils/router.js
+++ src/Utils/router.js
@@ -1,6 +1,15 @@
 const { Router } = require('express')
 
 const router = Router()
+// Criar usuarios
+// Fazer login
+// Fazer logout
+// Ver todas as fotos
+// Darlike em uma foto
+// Postar uma foto
+// Deletar uma foto
+// Editar uma foto
+// Visualizar Perfil do usuario
 
 router.get('/', (req, res) => {
     return res.send('Hellor word')
