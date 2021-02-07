7eec7e2d65e2571f3063b0b9230e1d9138717a45
lucasjunior24 <lukateku@gmail.com>
2021-02-07 13:39:09 -0300

    Add controller de login para as senhas

--- src/Controllers/LoginController.js
+++ src/Controllers/LoginController.js
@@ -7,8 +7,21 @@ module.exports = {
         try {
             const validUsername = await User.findOne({ username }) // saber se usuario existe
             // saber se são os mesmos username
+
             if (!validUsername) return res.status(400).send({message: 'Usuário não existe'})
-            return
+            const validPassword = await User.findOne({ 
+                password: password 
+            }).where({ 
+                username: username
+            })
+            // findOne = encontrar um
+            // onde paassord seja igual a password
+            //  se nao achar
+            if(!validPassword) return res.status(400).send({message: "Senha invalida"})
+
+            const loggedin = validPassword
+
+            return res.status(200).send({ message: 'sucesso', data: loggedin })
         } catch(err) {
             return res.status(400).send(err)
         }
