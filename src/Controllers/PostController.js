4d150224bf525aa79a0e3c8546973a769aad81a1
lucasjunior24 <lukateku@gmail.com>
2021-02-07 14:14:27 -0300

    Add controller de criar Posts

--- src/Controllers/PostController.js
+++ src/Controllers/PostController.js
@@ -2,6 +2,25 @@ const Post = require('../Models/Post')
 
 module.exports = {
     async createPost(req, res) {
+        const { 
+            picture,
+            description
+         } = req.body
+         const { user } = req.headers
+
+         try {
+             const newPost = await Post.create({
+                 picture,
+                 description,
+                 user
+             })
+             return res.status(200).send({
+                 message: "Post criado com sucesso",
+                 data: newPost
+             })
+         } catch(err) {
+             return res.status(400).send(err)
+         }
 
     },
     async listAllPosts(req, res) {
@@ -11,6 +30,6 @@ module.exports = {
 
     },
     async editPost(req, res) {
-        
+
     }
 }
