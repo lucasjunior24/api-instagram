5c789eda7ba1c94fd81026d0e12f79cfee1e9843
lucasjunior24 <lukateku@gmail.com>
2021-02-13 18:20:45 -0300

    Pegando posts por usuario

--- src/Controllers/ProfileController.js
+++ src/Controllers/ProfileController.js
@@ -1,16 +1,24 @@
 const User = require('../Models/User')
+const Post = require('../Models/Post')
 
 module.exports = {
     async listProfile(req, res) {
         const { user_id } = req.params
+        
 
         try { 
             const userInfo = await User.findById(user_id)
-            console.log(userInfo)
+            if(!userInfo) return res.status(400).send({
+                message: 'Usuario não existe'
+            })
+            const userPost = await Post.find({
+                user: user_id
+            }) // trazer todos os posts onde o id do usuario seja igual o id recebido pelo parametro
 
             return res.status(200).send({
-                message: 'Informações do Usuario',
-                data: userInfo
+                message: 'Usuario encontrado',
+                userInfo,
+                userPost
             })
         } catch(err) {
             return res.status(400).send(err)
