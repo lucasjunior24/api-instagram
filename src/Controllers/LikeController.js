ccdde22aac75f8706f5d83992e9f799635fb40bc
lucasjunior24 <lukateku@gmail.com>
2021-02-14 14:51:49 -0300

    Salvando likes

--- src/Controllers/LikeController.js
+++ src/Controllers/LikeController.js
@@ -7,6 +7,12 @@ module.exports = {
 
         try {
             const likedPost = await Post.findById(post_id)
+            if(!likedPost) return res.status(400).send('Este post não existe!')
+
+            if(likedPost.likes.includes(user_id)) return res.status(400).send('Este post já recebeu seu like')
+
+            likedPost.likes.push(user_id)
+            await likedPost.save()
 
             return res.status(200).send(likedPost)
         } catch(err) {
